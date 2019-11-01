e05028536f1f37593715306cb1cc35ca14bc0538
yangyanan <1607505226@qq.com>
2019-11-01 08:49:50 +0800

    提交了一个git测试项目

---
+++ test-GIT/src/main/java/com/example/HelloDemo.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * Created by yang on 2019/11/1.
+ */
+public class HelloDemo {
+	public static void main(String[] args) {
+		System.out.println("hello World");
+		System.out.println("hello World");
+		System.out.println("hello World");
+		System.out.println("hello World");
+		System.out.println("hello World");
+		System.out.println("hello World");
+		System.out.println("hello World");
+		System.out.println("hello World");
+	}
+
+
+}
